a65fe61492459887db89d78f82229975fd0a291a
mrbrunelli <matheus.brunelli@gmail.com>
2022-02-17 13:21:17 -0300

    feat: add new title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Matheus Brunelli",
+  title: "Programação e Carreira",
   tagline: "Blog Matheus Brunelli",
   url: "https://mrbrunelli.github.io",
   baseUrl: "/blog/",

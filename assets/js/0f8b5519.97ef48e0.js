931531d600ec75ba5bb4276e7f08c3e06576b6be
Matheus Ricardo Brunelli <matheus.brunelli@gmail.com>
2022-02-22 02:18:23 +0000

    deploy: 99c7f471064ff71b174b91d564ae7c722db11a95

---
+++ assets/js/0f8b5519.97ef48e0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[60],{989:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/every","name":"every","count":1,"permalink":"/blog/tags/every"}')}}]);
---
+++ assets/js/92bfd047.df27fadb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[20],{6330:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/validador","name":"validador","count":1,"permalink":"/blog/tags/validador"}')}}]);
